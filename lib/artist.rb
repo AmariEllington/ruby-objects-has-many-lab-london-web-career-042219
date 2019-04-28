4b2628071e826a96b793faea3e57a2245929f015
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-28 20:28:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,5 +33,12 @@ def add_song_by_name(name)
     song.artist = self
     @@song_count += 1
   end
-  
+
+  def add_song_by_name(name)
+   song = Song.new(name)
+   self.songs << song
+   song.artist = self
+   @@song_count +=1
+ end
+ 
 end
